e7f2125af242fd521d33df3090b7651ba8284a57
Niha3579 <22h51a05k0@cmrcet.ac.in>
2024-11-23 18:04:38 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,99 @@
-javascript
-50
-import React from 'react';
-import TodoList from './components/TodoList';
-function App() {
- return (
- <div className="App">
- <TodoList />
- </div>
- );
-}
+import React, { useState } from 'react';
+import './App.css';
+
+// TodoItem component - Displays individual todo
+const TodoItem = ({ todo, toggleComplete, deleteTodo }) => {
+  return (
+    <div className="todo-item">
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => toggleComplete(todo.id)}
+      />
+      <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+        {todo.text}
+      </span>
+      <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+    </div>
+  );
+};
+
+// TodoList component - Displays the list of todo items
+const TodoList = ({ todos, toggleComplete, deleteTodo }) => {
+  return (
+    <div>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          toggleComplete={toggleComplete}
+          deleteTodo={deleteTodo}
+        />
+      ))}
+    </div>
+  );
+};
+
+// AddTodo component - Adds a new todo item
+const AddTodo = ({ addTodo }) => {
+  const [input, setInput] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (input.trim()) {
+      addTodo(input);
+      setInput('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add a new todo"
+      />
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+};
+
+// App component - Main component managing state and logic
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  // Add todo
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Date.now(),
+      text: text,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  // Toggle completion status of todo
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  // Delete a todo
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div className="app">
+      <h1>TODO App</h1>
+      <AddTodo addTodo={addTodo} />
+      <TodoList todos={todos} toggleComplete={toggleComplete} deleteTodo={deleteTodo} />
+    </div>
+  );
+};
+
 export default App;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,20 +1,20 @@
-javascript
-import React from 'react';
-const Todo = ({ todo, toggleComplete, removeTodo }) => {
- return (
- <div style={{ display: 'flex', alignItems: 'center' }}>
- <input
- type="checkbox"
- checked={todo.completed}
- onChange={() => toggleComplete(todo.id)}
- />
- <span style={{ textDecoration: todo.completed ? 'line-through' : 'none',
-marginLeft: '10px' }}>
- {todo.text}
- </span>
- <button onClick={() => removeTodo(todo.id)} style={{ marginLeft: 'auto' }}>
- Remove
- </button>
- </div>
- );
-};
+// javascript
+// import React from 'react';
+// const Todo = ({ todo, toggleComplete, removeTodo }) => {
+//  return (
+//  <div style={{ display: 'flex', alignItems: 'center' }}>
+//  <input
+//  type="checkbox"
+//  checked={todo.completed}
+//  onChange={() => toggleComplete(todo.id)}
+//  />
+//  <span style={{ textDecoration: todo.completed ? 'line-through' : 'none',
+// marginLeft: '10px' }}>
+//  {todo.text}
+//  </span>
+//  <button onClick={() => removeTodo(todo.id)} style={{ marginLeft: 'auto' }}>
+//  Remove
+//  </button>
+//  </div>
+//  );
+// };
